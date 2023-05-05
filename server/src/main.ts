26387980437ef47548522c797a0e5414fb8b4b93
PrPedrosa <prpedrosa95@gmail.com>
2023-05-05 19:57:06 +0100

    updates cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: {}
+    cors: { origin: `${process.env.ORIGIN}` }
   })
 
   await app.listen(3000)
