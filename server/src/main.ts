9b09ca1ae06802068ef372d796e29508a96774e2
PrPedrosa <prpedrosa95@gmail.com>
2023-05-06 14:34:29 +0100

    testing without cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-require('dotenv').config()
+//require('dotenv').config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create(
+    AppModule /* , {
     cors: { origin: `${process.env.ORIGIN}` }
-  })
+  } */
+  )
 
   await app.listen(3000)
 }
