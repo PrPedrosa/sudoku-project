f45eaf2effa5859cbfa0cdaa0f0cdae53a848848
Pedro <s02251@newday.co.uk>
2023-09-20 11:31:15 +0100

    styles improv

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,177 +1,170 @@
 import { useEffect, useState } from "react"
-import { Board } from "./pages/Board"
-import { generateSudoku } from "./scripts/createBoard"
-import { Square } from "./types"
-import { Home } from "./pages/Home"
-import { updateSuperPositions } from "./scripts/updateSuperPositions"
-import { GoBackButton } from "./components/GoBackButton"
-import { Timer } from "./components/Timer"
-import { CheckSolutionModal } from "./components/CheckSolutionModal"
-import { SudokuSolver } from "./pages/SudokuSolver"
-import { SolverModeButton } from "./components/SolverModeButton"
-import { handleHiScoreStorage } from "./scripts/handleHiScoreStorage"
-import { cx } from "./utils"
 import { addVisit } from "./api/metrics"
+import { CheckSolutionModal } from "./components/CheckSolutionModal"
+import { GoBackButton } from "./components/GoBackButton"
+import { SolverModeButton } from "./components/SolverModeButton"
+import { Timer } from "./components/Timer"
+import { Board } from "./pages/Board"
+import { Home } from "./pages/Home"
+import { SudokuSolver } from "./pages/SudokuSolver"
+import { generateSudoku } from "./scripts/createBoard"
+import { handleHiScoreStorage } from "./scripts/handleHiScoreStorage"
+import { updateSuperPositions } from "./scripts/updateSuperPositions"
+import { Square } from "./types"
+import { cx } from "./utils"
 
 function App() {
-	const [board, setBoard] = useState<Square[]>()
-	const [initialSquaresIds, setInitialSquareIds] = useState<number[]>()
-	const [solvedBoard, setSolvedBoard] = useState<Square[]>()
-	const [playing, setPlaying] = useState(false)
-	const [timer, setTimer] = useState<number>(0)
-	const [intervalId, setIntervalId] = useState<NodeJS.Timer>()
-	const [retry, setRetry] = useState(false)
-	const [solution, setSolution] = useState(false)
-	const [solverMode, setSolverMode] = useState(false)
-	const [mode, setMode] = useState<"easy" | "medium" | "hard">()
-	const [introAnimationCounter, setIntroAnimationCounter] = useState(1)
-	const [visitCounter, setVisitCounter] = useState(0)
+  const [board, setBoard] = useState<Square[]>()
+  const [initialSquaresIds, setInitialSquareIds] = useState<number[]>()
+  const [solvedBoard, setSolvedBoard] = useState<Square[]>()
+  const [playing, setPlaying] = useState(false)
+  const [timer, setTimer] = useState<number>(0)
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer>()
+  const [retry, setRetry] = useState(false)
+  const [solution, setSolution] = useState(false)
+  const [solverMode, setSolverMode] = useState(false)
+  const [mode, setMode] = useState<"easy" | "medium" | "hard">()
+  const [introAnimationCounter, setIntroAnimationCounter] = useState(1)
+  const [visitCounter, setVisitCounter] = useState(0)
 
-	useEffect(() => {
-		if (!visitCounter) {
-			addVisit()
-			setVisitCounter(1)
-		}
+  useEffect(() => {
+    if (!visitCounter) {
+      addVisit()
+      setVisitCounter(1)
+    }
 
-		const storage = ["easy-scores", "medium-scores", "hard-scores"]
-		storage.forEach(item =>
-			localStorage.getItem(item)
-				? undefined
-				: localStorage.setItem(item, "0,0,0")
-		)
-	}, [])
+    const storage = ["easy-scores", "medium-scores", "hard-scores"]
+    storage.forEach((item) =>
+      localStorage.getItem(item)
+        ? undefined
+        : localStorage.setItem(item, "0,0,0")
+    )
+  }, [])
 
-	function handleGetRandomBoard(difficulty: "easy" | "medium" | "hard") {
-		if (!difficulty) return
-		setMode(difficulty)
-		setPlaying(true)
+  function startPlaying(difficulty: "easy" | "medium" | "hard") {
+    if (!difficulty) return
+    setMode(difficulty)
+    setPlaying(true)
 
-		const { unsolvedBoard, solvedBoard } = generateSudoku(difficulty)
-		setBoard(unsolvedBoard)
-		setSolvedBoard(solvedBoard)
+    const { unsolvedBoard, solvedBoard } = generateSudoku(difficulty)
+    setBoard(unsolvedBoard)
+    setSolvedBoard(solvedBoard)
 
-		const initialSqs = unsolvedBoard
-			.filter(sq => sq.value !== 0)
-			.map(sq => sq.id)
-		setInitialSquareIds(initialSqs)
+    const initialSqs = unsolvedBoard
+      .filter((sq) => sq.value !== 0)
+      .map((sq) => sq.id)
+    setInitialSquareIds(initialSqs)
 
-		const intId = setInterval(() => {
-			setTimer(prev => prev + 1)
-		}, 1000)
-		setIntervalId(intId)
-	}
+    const intId = setInterval(() => {
+      setTimer((prev) => prev + 1)
+    }, 1000)
+    setIntervalId(intId)
+  }
 
-	function handleInput(squareId: number, value: number) {
-		if (!board) return
-		const newBoard = board.map(sq => {
-			if (squareId === sq.id) return { ...sq, value: value }
-			return sq
-		})
-		setBoard(updateSuperPositions(newBoard))
-	}
+  function handleInput(squareId: number, value: number) {
+    if (!board) return
+    const newBoard = board.map((sq) => {
+      if (squareId === sq.id) return { ...sq, value: value }
+      return sq
+    })
+    setBoard(updateSuperPositions(newBoard))
+  }
 
-	//Use in dev env only, pass solve func to Board solve prop
-	/* function solve() {
-		if (!board) return
-		setBoard(solvedBoard)
-	} */
+  const stopTimer = () => {
+    clearInterval(intervalId)
+    setTimer(0)
+    setIntervalId(undefined)
+  }
 
-	const stopTimer = () => {
-		clearInterval(intervalId)
-		setTimer(0)
-		setIntervalId(undefined)
-	}
+  function handleCheckSolution(time: number) {
+    const oldBoard = board
+    if (!board) return
+    const newBoard: Square[] = board.map((sq) => {
+      if (
+        !board.find(
+          (squ) =>
+            squ.value === sq.value &&
+            squ.id !== sq.id &&
+            (squ.rowId === sq.rowId ||
+              squ.colId === sq.colId ||
+              squ.boxId === sq.boxId)
+        )
+      ) {
+        return { ...sq, valid: true }
+      } else return { ...sq, valid: false }
+    })
 
-	function handleCheckSolution(time: number) {
-		const oldBoard = board
-		if (!board) return
-		const newBoard: Square[] = board.map(sq => {
-			if (
-				!board.find(
-					squ =>
-						squ.value === sq.value &&
-						squ.id !== sq.id &&
-						(squ.rowId === sq.rowId ||
-							squ.colId === sq.colId ||
-							squ.boxId === sq.boxId)
-				)
-			) {
-				return { ...sq, valid: true }
-			} else return { ...sq, valid: false }
-		})
+    if (newBoard.find((sq) => sq.valid === false)) {
+      setBoard(newBoard)
+      setRetry(true)
+      setTimeout(() => {
+        setBoard(oldBoard)
+        setRetry(false)
+      }, 1500)
+      return
+    }
+    handleHiScoreStorage(time, mode)
+    setBoard(newBoard)
+    setRetry(false)
+    setSolution(true)
+  }
 
-		if (newBoard.find(sq => sq.valid === false)) {
-			setBoard(newBoard)
-			setRetry(true)
-			setTimeout(() => {
-				setBoard(oldBoard)
-				setRetry(false)
-			}, 1500)
-			return
-		}
-		handleHiScoreStorage(time, mode)
-		setBoard(newBoard)
-		setRetry(false)
-		setSolution(true)
-	}
+  function handleGoBack() {
+    setPlaying(false)
+    setBoard(undefined)
+    setSolvedBoard(undefined)
+    setSolution(false)
+    setRetry(false)
+    setSolverMode(false)
+    stopTimer()
+  }
 
-	function handleGoBack() {
-		setPlaying(false)
-		setBoard(undefined)
-		setSolvedBoard(undefined)
-		setSolution(false)
-		setRetry(false)
-		setSolverMode(false)
-		stopTimer()
-	}
+  const isBoardNotCompleted = () => board?.find((sq) => sq.value === 0)
 
-	const isBoardNotCompleted = () => board?.find(sq => sq.value === 0)
+  return (
+    <div
+      className={cx(
+        "h-[100vh] bg-c-dark1 flex w-[100%] items-center justify-center relative overflow-auto"
+      )}
+    >
+      <GoBackButton
+        playing={playing}
+        solverMode={solverMode}
+        goBack={handleGoBack}
+      />
+      <Timer time={timer} playing={playing} />
 
-	return (
-		<div
-			className={cx(
-				"h-[100vh] bg-c-dark1 flex w-[100%] items-center justify-center relative overflow-auto"
-			)}
-		>
-			<GoBackButton
-				playing={playing}
-				solverMode={solverMode}
-				goBack={handleGoBack}
-			/>
-			<Timer time={timer} playing={playing} />
+      {!playing && !solverMode && (
+        <SolverModeButton setSolverMode={setSolverMode} />
+      )}
+      {!playing && solverMode && <SudokuSolver />}
+      {!playing && !solverMode && (
+        <Home
+          getBoard={startPlaying}
+          animate={[introAnimationCounter, setIntroAnimationCounter]}
+        />
+      )}
 
-			{!playing && !solverMode && (
-				<SolverModeButton setSolverMode={setSolverMode} />
-			)}
-			{!playing && solverMode && <SudokuSolver />}
-			{!playing && !solverMode && (
-				<Home
-					getBoard={handleGetRandomBoard}
-					animate={[introAnimationCounter, setIntroAnimationCounter]}
-				/>
-			)}
+      {playing && (
+        <Board
+          board={board}
+          handleInput={handleInput}
+          initialSquares={initialSquaresIds}
+        />
+      )}
 
-			{playing && (
-				<Board
-					board={board}
-					handleInput={handleInput}
-					initialSquares={initialSquaresIds}
-					/* solve={solve} */
-				/>
-			)}
-
-			{playing && !isBoardNotCompleted() && (
-				<CheckSolutionModal
-					solution={solution}
-					retry={retry}
-					checkSolution={handleCheckSolution}
-					time={timer}
-					mode={mode}
-					goBack={handleGoBack}
-				/>
-			)}
-		</div>
-	)
+      {playing && !isBoardNotCompleted() && (
+        <CheckSolutionModal
+          solution={solution}
+          retry={retry}
+          checkSolution={handleCheckSolution}
+          time={timer}
+          mode={mode}
+          goBack={handleGoBack}
+        />
+      )}
+    </div>
+  )
 }
 
 export default App
--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -5,92 +5,92 @@ import { Board as BoardType, Square as SquareType } from "../types"
 import { cx } from "../utils"
 
 export function Board({
-	board,
-	handleInput,
-	initialSquares,
-	solve
+  board,
+  handleInput,
+  initialSquares,
+  solve,
 }: {
-	board?: BoardType
-	handleInput: (squareId: number, value: number) => void
-	initialSquares?: number[]
-	solve?: () => void
+  board?: BoardType
+  handleInput: (squareId: number, value: number) => void
+  initialSquares?: number[]
+  solve?: () => void
 }) {
-	if (!board) return null
+  if (!board) return null
 
-	const [selectedSquare, setSelectedSquare] = useState<SquareType>()
-	const inputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-	const [selectedSquareId, value, setValue] =
-		useKeyboardControls(selectedSquare)
+  const [selectedSquare, setSelectedSquare] = useState<SquareType>()
+  const inputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const [selectedSquareId, value, setValue] =
+    useKeyboardControls(selectedSquare)
 
-	useEffect(() => {
-		selectSquare(board[selectedSquareId - 1])
-		setSelectedSquare(board[selectedSquareId - 1])
-	}, [selectedSquareId])
+  useEffect(() => {
+    selectSquare(board[selectedSquareId - 1])
+    setSelectedSquare(board[selectedSquareId - 1])
+  }, [selectedSquareId])
 
-	useEffect(() => {
-		if (value === undefined) return
-		selectInput(value)
-		setValue(undefined)
-	}, [value])
+  useEffect(() => {
+    if (value === undefined) return
+    selectInput(value)
+    setValue(undefined)
+  }, [value])
 
-	const selectSquare = (sq: SquareType) => {
-		if (sq.id === selectedSquare?.id) setSelectedSquare(undefined)
-		else setSelectedSquare(sq)
-	}
+  const selectSquare = (sq: SquareType) => {
+    if (sq.id === selectedSquare?.id) setSelectedSquare(undefined)
+    else setSelectedSquare(sq)
+  }
 
-	const selectInput = (value: number) => {
-		if (!selectedSquare || initialSquares?.includes(selectedSquare.id)) return
-		handleInput(selectedSquare.id, value)
-	}
+  const selectInput = (value: number) => {
+    if (!selectedSquare || initialSquares?.includes(selectedSquare.id)) return
+    handleInput(selectedSquare.id, value)
+  }
 
-	const selectedSquareSiblings = board.filter(
-		sq =>
-			sq.id !== selectedSquare?.id &&
-			(sq.boxId === selectedSquare?.boxId ||
-				sq.colId === selectedSquare?.colId ||
-				sq.rowId === selectedSquare?.rowId)
-	)
+  const selectedSquareSiblings = board.filter(
+    (sq) =>
+      sq.id !== selectedSquare?.id &&
+      (sq.boxId === selectedSquare?.boxId ||
+        sq.colId === selectedSquare?.colId ||
+        sq.rowId === selectedSquare?.rowId)
+  )
 
-	return (
-		<div className='p-[5px] w-[100%] flex flex-col items-center justify-center gap-[15px] relative'>
-			{solve && (
-				<div
-					className='text-white p-[4px] absolute top-[-50px] right-[50%] translate-x-[50%] bg-c-dark2 border border-c-purple rounded-[5px] shadow-button-purple active:bg-c-purple cursor-pointer'
-					onClick={solve}
-				>
-					Solve!
-				</div>
-			)}
+  return (
+    <div className='p-[5px] w-[100%] flex flex-col items-center justify-center gap-[15px] relative'>
+      {solve && (
+        <div
+          className='text-white p-[4px] absolute top-[-50px] right-[50%] translate-x-[50%] bg-c-dark2 border border-c-purple rounded-[5px] shadow-button-purple active:bg-c-purple cursor-pointer'
+          onClick={solve}
+        >
+          Solve!
+        </div>
+      )}
 
-			<div className='grid grid-cols-9 border-[3px] sm:w-[450px] border-black rounded-[5px] shadow-board w-[100%] relative'>
-				{board.map(sq => {
-					return (
-						<Square
-							sq={sq}
-							key={sq.id}
-							select={selectSquare}
-							selected={selectedSquare?.id === sq.id}
-							isInitial={initialSquares?.includes(sq.id)}
-							isSibling={selectedSquareSiblings.includes(sq)}
-						/>
-					)
-				})}
-			</div>
-			<div className='grid grid-cols-10 w-[100%] text-center gap-[5px] sm:w-[450px]'>
-				{inputs.map(i => (
-					<div
-						className={cx(
-							"border border-black px-[8px] text-c-purple text-[20px] font-semibold rounded-[5px] bg-c-dark2 shadow-button-purple select-none text-center cursor-pointer",
-							"active:bg-c-purple active:text-black",
-							{ "text-red-700": i === 0, "active:bg-red-700": i === 0 }
-						)}
-						onClick={() => selectInput(i)}
-						key={i}
-					>
-						{i !== 0 ? i : "0"}
-					</div>
-				))}
-			</div>
-		</div>
-	)
+      <div className='grid grid-cols-9 border-[3px] sm:w-[450px] border-black rounded-[5px] shadow-board w-[100%] relative'>
+        {board.map((sq) => {
+          return (
+            <Square
+              sq={sq}
+              key={sq.id}
+              select={selectSquare}
+              selected={selectedSquare?.id === sq.id}
+              isInitial={initialSquares?.includes(sq.id)}
+              isSibling={selectedSquareSiblings.includes(sq)}
+            />
+          )
+        })}
+      </div>
+      <div className='grid grid-cols-10 w-[100%] text-center gap-[5px] sm:w-[450px]'>
+        {inputs.map((i) => (
+          <div
+            className={cx(
+              "border border-black px-[8px] text-c-purple text-[24px] font-semibold rounded-[5px] bg-c-dark2 shadow-button-purple select-none text-center cursor-pointer",
+              "active:bg-c-purple active:text-black",
+              { "text-red-700": i === 0, "active:bg-red-700": i === 0 }
+            )}
+            onClick={() => selectInput(i)}
+            key={i}
+          >
+            {i !== 0 ? i : "0"}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
